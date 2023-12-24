0e312107ef2960a3728d9b97a5d6095343d52dae
macrijuan <amacri162013@yahoo.com>
2023-12-24 16:03:45 -0300

    test16

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const serverless = require("serverless-http");
 
 router.get('/', async(req,res)=>{
-  return new Response("App is running");
+  res.status(200).send("App is running");
 });
 
 router.get('/test', async(req,res)=>{
-  return res.json({message:'This is a JSON test'});
+  res.status(200).json({message:'This is a JSON test'});
 });
 
 router.use('/.netlify/functions/api', router);
 
-exports.handler = serverless(app);
+exports.handler = app;
