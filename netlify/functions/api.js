5335386bd6fbdc872d09eadada9d924522305344
macrijuan <amacri162013@yahoo.com>
2023-12-24 16:30:09 -0300

    I belive this is the good one

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -11,14 +11,7 @@ router.get('/test', async(req,res)=>{
   res.json({message:'This is a JSON test'});
 });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://stirring-jalebi-cda26f.netlify.app/'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
-router.use('/.netlify/functions/api', router);
+app.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app);
