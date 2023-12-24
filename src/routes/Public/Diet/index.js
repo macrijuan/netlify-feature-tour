12f3d4e9df59e75f3500455ab445c1f14115b160
macrijuan <amacri162013@yahoo.com>
2023-12-24 19:43:46 -0300

    Update: src/routes/Admin/Table/Get/index.js

--- src/routes/Public/Diet/index.js
+++
@@ -1,7 +0,0 @@
-const {Router}=require("express");
-const router = Router();
-const getDiet = require("../../Public/Diet/Get");
-
-router.use("/diet", getDiet);
-
-module.exports.handler = router;
--- src/routes/Public/Dish/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const getDish = require("./Get/index.js");
-
-//getDish SENDS ONE OR MULTIPLE DISHES
-router.use("/dish", getDish );
-
-module.exports.handler = router;
