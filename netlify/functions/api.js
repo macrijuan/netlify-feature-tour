5d11ddfa909f2bb75f2f3e3117c5d11f79c1b38d
macrijuan <amacri162013@yahoo.com>
2023-12-24 17:19:54 -0300

    database connection test1

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 const routes = require("../../routes/index.js");
+const router = express.Router();
 const serverless = require("serverless-http");
+const { Sequelize } = require('sequelize');
 
 
-app.use('/.netlify/functions/api', routes.handler);
+const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`, {
+  logging: false,//set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
+
+sequelize.authenticate().then(()=>{
+  app.use('/.netlify/functions/api', routes.handler);
+}).catch(err=>{
+  router.use((req,res)=>{
+    res.json({errors:{unknown:"Oh, no! There was a problem."}})
+  });
+});
 
 module.exports.handler = serverless(app);
