a4dacc34ee63adf92590bc033b4d63c1b7351983
macrijuan <amacri162013@yahoo.com>
2023-12-24 15:40:02 -0300

    test10

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -3,8 +3,12 @@ const app = express();
 const router = express.Router();
 const serverless = require("serverless-http");
 
-router.get('/test', async(req,res)=>{
-  res.json({message:"Hello world!"});
+router.get('/', (req,res)=>{
+  res.send("Hello world!");
+});
+
+router.get('/test', (req,res)=>{
+  res.json({message:"This is a JSON test"});
 });
 
 router.use('/.netlify/functions/index', router);
