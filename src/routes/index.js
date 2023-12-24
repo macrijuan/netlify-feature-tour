7e9c5e9296cf031ff09434562a7379f278c887d2
macrijuan <amacri162013@yahoo.com>
2023-12-24 20:04:14 -0300

    test1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
 const router = Router();
+// const {Admin} = require("../db.js");
 
 
-const admin = require("./Admin");
+
+// const admin = require("./Admin");
 
 
-router.use("/administrator", admin);
+// router.use("/administrator", admin);
+
+router.get("/get_modules", async(req,res)=>{
+  res.json({data:Admin});
+});
 
 module.exports.handler = router
