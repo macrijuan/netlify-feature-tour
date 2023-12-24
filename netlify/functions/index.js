77324e14017ef7168db40c41b64a3e6e31b411d5
macrijuan <amacri162013@yahoo.com>
2023-12-24 15:36:10 -0300

    test9

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -3,10 +3,10 @@ const app = express();
 const router = express.Router();
 const serverless = require("serverless-http");
 
-router.get("/test", async(req,res)=>{
+router.get('/test', async(req,res)=>{
   res.json({message:"Hello world!"});
 });
 
-router.use('https://stirring-jalebi-cda26f.netlify.app/.netlify/functions/index', router);
+router.use('/.netlify/functions/index', router);
 
 exports.handler = serverless(app);
