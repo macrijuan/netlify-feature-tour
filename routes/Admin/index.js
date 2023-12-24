1100ebc0950b64babbc476e965f205eaf66f6bb4
macrijuan <amacri162013@yahoo.com>
2023-12-24 16:40:00 -0300

    exportation method test2

--- routes/Admin/index.js
+++ routes/Admin/index.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/test", async(req,res)=>{
-  res.json("This is an admins-route");
+router.get("/administrator", async(req,res)=>{
+  res.send("This is an admins-route");
 });
 
 module.exports.handler = router;
--- routes/Public/index.js
+++ routes/Public/index.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/test", async(req,res)=>{
-  res.json("This is a public-route");
+router.get("/public", async(req,res)=>{
+  res.send("This is a public-route");
 });
 
 module.exports.handler = router;
