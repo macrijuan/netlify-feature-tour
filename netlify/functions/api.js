032eb66cf2b0885b77d0a8bb6137b3af9be6b0c1
macrijuan <amacri162013@yahoo.com>
2023-12-24 16:25:46 -0300

    test20

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -10,13 +10,15 @@ router.get('/', async(req,res)=>{
 router.get('/test', async(req,res)=>{
   res.json({message:'This is a JSON test'});
 });
-server.use((req, res, next) => {
+
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://stirring-jalebi-cda26f.netlify.app/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+
 router.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app);
