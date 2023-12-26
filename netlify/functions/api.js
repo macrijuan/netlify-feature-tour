69a25a80c4c7e723216454ee762c67a86b550618
macrijuan <amacri162013@yahoo.com>
2023-12-26 02:24:39 -0300

    test4

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,9 +6,9 @@ const { conn } = require("../../src/db.js").handler;
 const routes = require("../../src/routes/index.js").handler;
 
 conn.authenticate().then(()=>{
-  app.use('https://stirring-jalebi-cda26f.netlify.app/.netlify/functions/api', routes);
+  // app.use("/.netlify/functions/api", routes);
+  throw new Error('DB connected!');
 }).catch(()=>{
-  console.log('bye');
   throw new Error('DB not connected.');
 });
 
