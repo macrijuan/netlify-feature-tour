2dcf2601237e5f1976d999e5d9f43d814e133cff
macrijuan <amacri162013@yahoo.com>
2023-12-26 01:50:25 -0300

    Fix: delete all fs functions. They aren't needed.

--- src/db.js
+++ src/db.js
@@ -1,6 +1,4 @@
 const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('node:path');
 const pg = require('pg');
 require("dotenv").config();
 
@@ -9,28 +7,6 @@ const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env
   native: false,
   dialectModule: pg
 });
-
-const basename = path.basename(__filename);
-
-const modelDefiners = [];
-
-fs
-.readdirSync(path.join('/var/task/src', '/models'))
-.filter((file) => (
-  file.indexOf('.') !== 0) &&
-  (file !== basename) &&
-  (file.slice(-3) === '.js')
-)
-.forEach((file) => {
-  modelDefiners.push(require(path.join('/var/task/src', '/models', file)));
-});
-
-
-modelDefiners.forEach(model => model(sequelize));
-
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
-sequelize.models = Object.fromEntries(capsEntries);
 
 module.exports.handler = {
   ...sequelize.models,
