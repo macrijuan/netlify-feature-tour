85a1cfd0cebffe0dfb4c5234848d5727a8e556c9
macrijuan <amacri162013@yahoo.com>
2023-12-26 02:22:15 -0300

    test3

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
 const serverless = require("serverless-http");
 
 const { conn } = require("../../src/db.js").handler;
 const routes = require("../../src/routes/index.js").handler;
 
-
-
 conn.authenticate().then(()=>{
-  console.log('hello');
-  router.get("/", (req,res)=>{res.send('API status: OK')});
-  app.use('/.netlify/functions/api', routes);
+  app.use('https://stirring-jalebi-cda26f.netlify.app/.netlify/functions/api', routes);
 }).catch(()=>{
   console.log('bye');
   throw new Error('DB not connected.');
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-const {Admin} = require("../db.js").handler;
 
 // const admin = require("./Admin");
 
 // router.use("/administrator", admin);
+
+router.get("/", (req,res)=>{res.send('API status: OK')});
 
 router.get("/get_modules", (req,res)=>{
   res.json({data:"Admin.tableName"});
