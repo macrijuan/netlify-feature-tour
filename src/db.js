f74416966b8ee6e19e3f66637302a11c21df8f65
macrijuan <amacri162013@yahoo.com>
2023-12-26 01:46:44 -0300

    Fix: db.js fs methods

--- src/db.js
+++ src/db.js
@@ -15,14 +15,14 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 fs
-.readdirSync(path.join(__dirname, '/models'))
+.readdirSync(path.join('/var/task/src', '/models'))
 .filter((file) => (
   file.indexOf('.') !== 0) &&
   (file !== basename) &&
   (file.slice(-3) === '.js')
 )
 .forEach((file) => {
-  modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  modelDefiners.push(require(path.join('/var/task/src', '/models', file)));
 });
 
 
