1d3180b77866791e0e33a82a03194e9e27435361
macrijuan <amacri162013@yahoo.com>
2023-12-26 03:09:56 -0300

    test11

--- src/db.js
+++ src/db.js
@@ -2,10 +2,6 @@ const { Sequelize } = require('sequelize');
 const pg = require('pg');
 require("dotenv").config();
 
-////////////////////////////////////
-
-const { Sequelize } = require("sequelize");
-
 let sequelize = null;
 
 console.log(process.env.DB_USER);
@@ -21,32 +17,22 @@ async function loadSequelize() {
       evict: process.env.CURRENT_LAMBDA_FUNCTION_TIMEOUT
     }
   });
-
-  // or `sequelize.sync()`
   await sequelize.authenticate();
-
   return sequelize;
-}
+};
 
 module.exports.handler = async function (event, callback) {
-  // re-use the sequelize instance across invocations to improve performance
   if (!sequelize) {
     sequelize = await loadSequelize();
   } else {
-    // restart connection pool to ensure connections are not re-used across invocations
     sequelize.connectionManager.initPools();
-
-    // restore `getConnection()` if it has been overwritten by `close()`
     if (sequelize.connectionManager.hasOwnProperty("getConnection")) {
       delete sequelize.connectionManager.getConnection;
     }
   }
-
   try {
     return await doSomethingWithSequelize(sequelize);
   } finally {
-    // close any opened connections during the invocation
-    // this will wait for any in-progress queries to finish before closing the connections
     await sequelize.connectionManager.close();
   }
 };
