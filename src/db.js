f151f0107a116e6e9a1001d198c871e11712f7c8
macrijuan <amacri162013@yahoo.com>
2023-12-25 22:44:21 -0300

    DELETE: unnecesary table associations

--- src/db.js
+++ src/db.js
@@ -31,21 +31,6 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { Diet, Dish, User, Reservation, Table } = sequelize.models;
-
-Reservation.hasOne( User );
-
-
-User.hasMany( Reservation );
-Table.hasMany( Reservation );
-
-
-Reservation.hasOne( Table, { foreignKey:"ticket reserve", as:"ticket reserve" } );
-
-
-Diet.belongsToMany( Dish, { through:"dish_diets", timestamps:false } );
-Dish.belongsToMany( Diet, { through:"dish_diets", timestamps:false } );
-
 module.exports.handler = {
   ...sequelize.models,
   conn: sequelize,
