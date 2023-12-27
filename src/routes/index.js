da3c81d35226b4ac511e2d074524391cf79a4b5b
macrijuan <amacri162013@yahoo.com>
2023-12-27 03:20:36 -0300

    Fix src\db.js --> tes2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-// const router = Router();
+const router = Router();
 // const Admin = require("../models/Admin.js").handler;
 const { Admin } = require("../db.js").handler;
 // const model = Admin(conn);
@@ -14,4 +14,4 @@ router.get("/get_model", (req,res)=>{
   res.json({data:Admin.name});
 });
 
-module.exports.handler = router
+module.exports.handler = router;
