b548e71f707be9a257cad482de4d692a796ff395
macrijuan <amacri162013@yahoo.com>
2023-12-27 02:24:46 -0300

    Fix src\db.js --> Test if readdir func does the job

--- src/db.js
+++ src/db.js
@@ -22,7 +22,7 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 fs
-.readdirSync(path.join(__dirname, '/models'))
+.readdir(path.join(__dirname, '/models'))
 .filter((file) => (
   file.indexOf('.') !== 0) &&
   (file !== basename) &&
