7ffb8af34ca25aa8713960668fb6846bf7fd0bc5
macrijuan <amacri162013@yahoo.com>
2023-12-27 01:18:27 -0300

    test16

--- src/db.js
+++ src/db.js
@@ -16,7 +16,29 @@ const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env
   }
 });
 
-console.log(sequelize);
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+fs
+.readdir(path.join(__dirname, '/models'))
+.filter((file) => (
+  file.indexOf('.') !== 0) &&
+  (file !== basename) &&
+  (file.slice(-3) === '.js')
+)
+.forEach((file) => {
+  modelDefiners.push(require(path.join(__dirname, '/models', file)));
+});
+
+modelDefiners.forEach(model => model(sequelize));
+
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => {
+  // console.log(entry); 
+  return [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]
+});
+sequelize.models = Object.fromEntries(capsEntries);
 
 module.exports.handler = sequelize;
 
