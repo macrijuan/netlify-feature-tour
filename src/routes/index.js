91755f741fcc93c18eff7be0f1d011ac07d1bbc2
macrijuan <amacri162013@yahoo.com>
2023-12-27 05:10:35 -0300

    functional test 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,9 @@ const { Admin } = require("../db.js").handler;
 
 router.get("/", (req,res)=>{res.send('API status: OK')});
 
-router.get("/get_model", (req,res)=>{
-  res.json({data:Admin.findAll()});
+router.get("/get_model", async(req,res)=>{
+  Admin.findAll().then(admins=>res.json({data: admins}));
+  
 });
 
 module.exports.handler = router;
