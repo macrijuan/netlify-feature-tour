80858625878c90b7386572471b2b6e1da4815556
macrijuan <amacri162013@yahoo.com>
2023-12-27 21:50:47 -0300

    test2

--- src/routes/Admin/Admin/Post/Controllers/existing.js
+++ src/routes/Admin/Admin/Post/Controllers/existing.js
@@ -1,5 +1,5 @@
+const { Router }=require("express");
 const router = Router();
-const {Router}=require("express");
 const {Admin} = require("../../../../../db").handler;
 const {existing} = require("../../../../error").handler;
 
