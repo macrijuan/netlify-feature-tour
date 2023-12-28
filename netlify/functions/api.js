4ef135f2f034ff4c71db35615eaa0619b21b2f7c
macrijuan <amacri162013@yahoo.com>
2023-12-28 15:51:01 -0300

    Update netlify\functions\api.js --> Add error logging

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,7 +6,7 @@ const { conn } = require("../../src/db.js").handler;
 const routes = require("../../src/routes/index.js").handler;
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://molino-admin-five.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://molino-admin-five.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -18,5 +18,12 @@ app.use("/.netlify/functions/api",
   conn.authenticate().then(()=>{next();}).catch(()=>{res.send("DB connection failed.");});
 },
 routes);
+
+server.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports.handler = serverless(app);
