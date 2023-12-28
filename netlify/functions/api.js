a3cbb3528807ffb2d9d28ad93f578661ebb4d37d
macrijuan <amacri162013@yahoo.com>
2023-12-28 15:48:44 -0300

    Update --> Add headers to every response

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,6 +5,14 @@ const serverless = require("serverless-http");
 const { conn } = require("../../src/db.js").handler;
 const routes = require("../../src/routes/index.js").handler;
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://molino-admin-five.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use("/.netlify/functions/api",
 (req, res, next)=>{
   conn.authenticate().then(()=>{next();}).catch(()=>{res.send("DB connection failed.");});
