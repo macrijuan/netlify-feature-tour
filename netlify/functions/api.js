93b9750650bce7af84f7cbc72dfcf8abc0365821
macrijuan <amacri162013@yahoo.com>
2023-12-28 23:36:51 -0300

    Fix netlify\functions\api.js --> correct "server" identifier for "app"

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,7 +5,7 @@ const serverless = require("serverless-http");
 const { conn } = require("../../src/db.js").handler;
 const routes = require("../../src/routes/index.js").handler;
 
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://molino-admin-five.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
