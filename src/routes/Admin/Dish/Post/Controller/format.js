272d64af1549a57710cff7a7a553bc3929951417
macrijuan <amacri162013@yahoo.com>
2023-12-31 14:56:36 -0300

    test2

--- src/routes/Admin/Dish/Post/Controller/format.js
+++ src/routes/Admin/Dish/Post/Controller/format.js
@@ -16,12 +16,14 @@ router.use((req,res,next)=>{
   });
 	nameValidator(req.body.name, res.locals.errors);
 	ingredientsValidator(req.body.ingredients, res.locals.errors);
-  console.log(res.locals.errors);
 	dietsValidator(req.body.diets, res.locals.errors);
 	descriptionValidator(req.body.description, res.locals.errors);
   req.body.price = Number(req.body.price);
 	priceValidator(req.body.price, res.locals.errors);
 	imageValidator(req.body.image, res.locals.errors);
+  
+  console.log(res.locals.errors);
+
   if(Object.keys(res.locals.errors).length){
     res.status(403).json({errors:res.locals.errors, post:true});
   }else{
