0dc4d5b238dba4096aa82374509f90509d4c131c
macrijuan <amacri162013@yahoo.com>
2024-01-03 13:32:49 -0300

    parse req.body

--- src/routes/Admin/index.js
+++ src/routes/Admin/index.js
@@ -9,6 +9,6 @@ const reservation = require("./Reservation").handler;
 const table = require("./Table").handler;
 const option = require("./Option").handler;
 
-router.use( ( req, res, next )=>{ req.body = JSON.parse( req.body ) }, user, dish, diet, admin, inventory, reservation, table, option );
+router.use( ( req, res, next )=>{   req.body = JSON.parse( req.body.toString() ); }, user, dish, diet, admin, inventory, reservation, table, option );
 
 module.exports.handler=router;
