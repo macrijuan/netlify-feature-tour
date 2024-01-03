45d08ac48a332de6ed6d4366c0d41e1bd15440e2
macrijuan <amacri162013@yahoo.com>
2024-01-03 14:22:10 -0300

    test6

--- src/routes/Admin/index.js
+++ src/routes/Admin/index.js
@@ -9,6 +9,13 @@ const reservation = require("./Reservation").handler;
 const table = require("./Table").handler;
 const option = require("./Option").handler;
 
-router.use( ( req, res, next )=>{ console.log( req.body ); next(); }, user, dish, diet, admin, inventory, reservation, table, option );
+router.use( 
+  ( req, res, next )=>{ 
+    const data = JSON.parse( req.body ); 
+    console.log( data ); 
+    next(); 
+  }, 
+  user, dish, diet, admin, inventory, reservation, table, option
+);
 
 module.exports.handler=router;
